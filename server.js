7a544495763c9fe25c82c529f7d4f49890cd0442
Elad Cohen <elad@codicate.com>
2021-08-25 11:35:51 +0300

    CRUD by DB

--- server.js
+++ server.js
@@ -23,52 +23,27 @@ app.get("/", (req, res) => {
 });
 
 app.get("/todos", (req, res) => {
-  fs.readFile("./todos.json", "utf8", (err, data) => {
-    if (err) {
-      fs.writeFile("./todos.json", "[]", (err) => {});
-      res.send([]);
-    } else {
-      const { title } = req.query;
+  const { title } = req.query;
 
-      let todos = JSON.parse(data);
-
-      if (title) {
-        todos = todos.filter((todo) =>
-          todo.title
-            ? todo.title.toLowerCase().includes(title.toLowerCase())
-            : false
-        );
-      }
-
-      res.send(todos);
+  Todo.find((err, todos) => {
+    if (title) {
+      todos = todos.filter((todo) =>
+        todo.title
+          ? todo.title.toLowerCase().includes(title.toLowerCase())
+          : false
+      );
     }
+
+    res.send(todos);
   });
 });
 
 app.get("/todos/:id", (req, res) => {
   const { id } = req.params;
-  fs.readFile("./todos.json", "utf8", (err, data) => {
-    const todos = JSON.parse(data);
-    const todo = todos.find((todo) => todo.id === +id);
+  Todo.findById(id, (err, todo) => {
     res.send(todo);
   });
 });
-
-// app.post("/todos", (req, res) => {
-//   const { title } = req.body;
-
-//   fs.readFile("./todos.json", "utf8", (err, data) => {
-//     const todos = JSON.parse(data);
-//     const newTodo = {
-//       id: uuidv4(),
-//       title,
-//       completed: false,
-//       userId: "1",
-//     };
-//     todos.push(newTodo);
-//     fs.writeFile("./todos.json", JSON.stringify(todos), (err) => {});
-//   });
-// });
 
 app.post("/todos", (req, res) => {
   const { title } = req.body;
@@ -78,73 +53,27 @@ app.post("/todos", (req, res) => {
   todo.save();
 
   res.send("OK!");
-
-  // fs.readFile("./todos.json", "utf8", (err, data) => {
-  //   const todos = JSON.parse(data);
-  //   const newTodo = {
-  //     id: uuidv4(),
-  //     title,
-  //     completed: false,
-  //     userId: "1",
-  //   };
-  //   todos.push(newTodo);
-  //   fs.writeFile("./todos.json", JSON.stringify(todos), (err) => {});
-  // });
-});
-
-app.post("/todos", (req, res) => {
-  const { title } = req.body;
-
-  fs.readFile("./todos.json", "utf8", (err, data) => {
-    const todos = JSON.parse(data);
-    const newTodo = {
-      id: uuidv4(),
-      title,
-      completed: false,
-      userId: "1",
-    };
-    todos.push(newTodo);
-    fs.writeFile("./todos.json", JSON.stringify(todos), (err) => {});
-    res.send("OK");
-  });
 });
 
 app.put("/todos/:id", (req, res) => {
   const { id } = req.params;
-  const { title } = req.body;
-  fs.readFile("./todos.json", "utf8", (err, data) => {
-    const todos = JSON.parse(data);
-    const todo = todos.find((todo) => todo.id === id);
-    if (todo) {
-      todo.title = title;
-      fs.writeFile("./todos.json", JSON.stringify(todos), (err) => {
-        res.send("OK");
-      });
-    } else {
-      res.send("Not found");
-    }
+  const { title, userId } = req.body;
+
+  const updatedFields = {};
+  title ? (updatedFields.title = title) : null;
+  userId ? (updatedFields.userId = userId) : null;
+
+  Todo.findByIdAndUpdate(id, updatedFields, (err, todo) => {
+    res.send(todo);
   });
 });
 
 app.delete("/todos/:id", (req, res) => {
   try {
     const { id } = req.params;
-    fs.readFile("./todos.json", "utf8", (err, data) => {
-      const todos = JSON.parse(data);
-
-      // METHOD 1:
-      const todoIndex = todos.findIndex((todo) => todo.id === id);
-      if (todoIndex > -1) {
-        todos.splice(todoIndex, 1);
-        fs.writeFile("./todos.json", JSON.stringify(todos), (err) => {
-          res.send("OK");
-        });
-      } else {
-        res.send("Not found");
-      }
-
-      // METHOD 2:
-      // const newTodos = todos.filter(todo=> todo.id !== id);
+    Todo.findByIdAndDelete(id, (err, todo) => {
+      console.log(todo);
+      res.send(todo);
     });
   } catch (error) {
     res.send("Not found");
@@ -156,12 +85,13 @@ app.get("*", (req, res) => {
 });
 
 function initTodos() {
-  fs.readFile("./todos.json", "utf8", (err, data) => {
-    if (!data) {
+  Todo.findOne((err, todo) => {
+    if (!todo) {
       fs.readFile("./initialTodos.json", "utf8", (err, data) => {
         let initialTodos = JSON.parse(data);
         initialTodos = initialTodos.map((todo) => ({ ...todo, id: uuidv4() }));
-        fs.writeFile("./todos.json", JSON.stringify(initialTodos), (err) => {});
+
+        Todo.insertMany(initialTodos, (err, todos) => {});
       });
     }
   });
